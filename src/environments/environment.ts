36cbb84a0d6d5bf82c226b0b25aa74ab248a3bf9
CristianF1904 <cristian.ferreira1904@gmail.com>
2023-09-19 18:12:57 -0300

    Integración de Firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'ficha2-app-e8e19',
+    appId: '1:534502676945:web:bc99ba792d5777b853e8f0',
+    databaseURL: 'https://ficha2-app-e8e19-default-rtdb.firebaseio.com',
+    storageBucket: 'ficha2-app-e8e19.appspot.com',
+    apiKey: 'AIzaSyDN7XM9cx7TlVSX7j4S9qdhW2QPFVUs5xo',
+    authDomain: 'ficha2-app-e8e19.firebaseapp.com',
+    messagingSenderId: '534502676945',
+  },
   production: false
 };
 
